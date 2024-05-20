22b412596c84f51181cb53de0ff06561b2212148
Андрей <79538233369@ya.ru>
2024-05-20 10:38:08 +0500

    master

---
+++ src/main/java/pro/sky/calculator_test/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/calculator_test/CalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
