430f3d57357417c8adeb4f6f4ce88010bb355f59
Андрей <79538233369@ya.ru>
2024-05-21 21:13:35 +0500

    changes

--- src/test/java/pro/sky/calculator_test/service/CalculatorServiceImplUnitTest.java
+++ src/test/java/pro/sky/calculator_test/service/CalculatorServiceImplUnitTest.java
@@ -2,13 +2,25 @@ package pro.sky.calculator_test.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.Arguments;
 import pro.sky.calculator_test.exceptions.ZeroDividerException;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorServiceImplUnitTest {
     CalculatorService calculatorService = new CalculatorServiceImpl();
+
+    List<Integer[]> testData = Arrays.asList(
+            new Integer[]{1, 2},
+            new Integer[]{2, 1},
+            new Integer[]{-1, 1},
+            new Integer[]{1, -1},
+            new Integer[]{-1, -1},
+            new Integer[]{0, 1},
+            new Integer[]{1, 0}
+    );
 
     @Test
     void hello() {
@@ -22,57 +34,43 @@ public class CalculatorServiceImplUnitTest {
 
     @Test
     void plus() {
-        Assertions.assertEquals(3, calculatorService.plus(1, 2));
-        Assertions.assertEquals(3, calculatorService.plus(2, 1));
-        Assertions.assertEquals(0, calculatorService.plus(-1, 1));
-        Assertions.assertEquals(0, calculatorService.plus(1, -1));
-        Assertions.assertEquals(-2, calculatorService.plus(-1, -1));
-        Assertions.assertEquals(1, calculatorService.plus(0, 1));
-        Assertions.assertEquals(1, calculatorService.plus(1, 0));
-        Assertions.assertEquals(2147483648L, calculatorService.plus(Integer.MAX_VALUE, 1));
-        Assertions.assertEquals(-2147483647, calculatorService.plus(Integer.MIN_VALUE, 1));
+        for (Integer[] data : testData) {
+            long expected = data[0] + data[1];
+            long actual = calculatorService.plus(data[0], data[1]);
+            assertEquals(expected, actual);
+        }
     }
+
 
     @Test
     void minus() {
-        Assertions.assertEquals(3, calculatorService.minus(1, 2));
-        Assertions.assertEquals(3, calculatorService.minus(2, 1));
-        Assertions.assertEquals(0, calculatorService.minus(-1, 1));
-        Assertions.assertEquals(0, calculatorService.minus(1, -1));
-        Assertions.assertEquals(-2, calculatorService.minus(-1, -1));
-        Assertions.assertEquals(1, calculatorService.minus(0, 1));
-        Assertions.assertEquals(1, calculatorService.minus(1, 0));
-        Assertions.assertEquals(2147483648L, calculatorService.minus(Integer.MAX_VALUE, 1));
-        Assertions.assertEquals(-2147483647, calculatorService.minus(Integer.MIN_VALUE, 1));
+        for (Integer[] data : testData) {
+            long expected = data[0] - data[1];
+            long actual = calculatorService.minus(data[0], data[1]);
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
     void multiply() {
-        Assertions.assertEquals(3, calculatorService.multiply(1, 2));
-        Assertions.assertEquals(3, calculatorService.multiply(2, 1));
-        Assertions.assertEquals(0, calculatorService.multiply(-1, 1));
-        Assertions.assertEquals(0, calculatorService.multiply(1, -1));
-        Assertions.assertEquals(-2, calculatorService.multiply(-1, -1));
-        Assertions.assertEquals(1, calculatorService.multiply(0, 1));
-        Assertions.assertEquals(1, calculatorService.multiply(1, 0));
-        Assertions.assertEquals(2147483648L, calculatorService.multiply(Integer.MAX_VALUE, 1));
-        Assertions.assertEquals(-2147483647, calculatorService.multiply(Integer.MIN_VALUE, 1));
+        for (Integer[] data : testData) {
+            long expected = (long) data[0] * data[1];
+            long actual = calculatorService.multiply(data[0], data[1]);
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
     void divide() {
-        Assertions.assertEquals(3, calculatorService.divide(1, 2));
-        Assertions.assertEquals(3, calculatorService.divide(2, 1));
-        Assertions.assertEquals(0, calculatorService.divide(-1, 1));
-        Assertions.assertEquals(0, calculatorService.divide(1, -1));
-        Assertions.assertEquals(-2, calculatorService.divide(-1, -1));
-        Assertions.assertEquals(2147483648L, calculatorService.divide(Integer.MAX_VALUE, 1));
-        Assertions.assertEquals(-2147483647, calculatorService.divide(Integer.MIN_VALUE, 1));
-    }
-
-    @Test
-    void divideByZeroTest() {
-        Assertions.assertThrows(ZeroDividerException.class, () -> calculatorService.divide(1, 0));
+        for (Integer[] data : testData) {
+            if (data[1] == 0) {
+                assertThrows(ZeroDividerException.class, () -> calculatorService.divide(data[0], data[1]));
+            } else {
+                long expected = data[0] / data[1];
+                long actual = (long) calculatorService.divide(data[0], data[1]);
+                assertEquals(expected, actual);
+            }
+        }
     }
 }
 
